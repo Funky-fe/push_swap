827b5e577d49b75fbdd929b7f494ad0c668b3cbd
Ruben Andre Lima Fernandes <rlima-fe@c1r9s11.42porto.com>
2023-10-17 16:50:33 +0100

    insanity

--- functions/ft_lst_create.c
+++ functions/ft_lst_create.c
@@ -6,7 +6,7 @@
 /*   By: rlima-fe <rlima-fe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 12:54:18 by rlima-fe          #+#    #+#             */
-/*   Updated: 2023/10/17 16:03:45 by rlima-fe         ###   ########.fr       */
+/*   Updated: 2023/10/17 16:43:27 by rlima-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 //    If it is, it means it is a quoted string.
 // 3. It checks if the number of input is greater than 2.
 //     If it is, it lists the arguements.
-int	ft_atoi2(const char *str)
+long	ft_atoi2(const char *str)
 {
 	int				mod;
 	long long int	i;
@@ -34,24 +34,24 @@ int	ft_atoi2(const char *str)
 		str++;
 	}
 	if (*str == '\0')
-		return (2147483648);
+		return (2147483649);
 	while (*str)
 	{
 		if (!ft_isdigit(*str))
-			return (2147483648);
+			return (2147483649);
 		i = i * 10 + (*str - 48);
 		str++;
 	}
 	if ((mod * i) > 2147483647 || (mod * i) < -2147483648)
-		return (2147483648);
+		return (2147483649);
 	return (mod * i);
 }
 
 t_stack	*ft_process(int argc, char **argv)
 {
-	t_stack	*a;
-	int		i;
-	int		j;
+	t_stack		*a;
+	long		i;
+	long		j;
 
 	i = 1;
 	a = NULL;
@@ -62,8 +62,11 @@ t_stack	*ft_process(int argc, char **argv)
 		while (i < argc)
 		{
 			j = ft_atoi2(argv[i]);
-			if (j == 2147483648)
+			if (j == 2147483649)
+			{
+				ft_free(&a);
 				ft_error();
+			}
 			ft_add_back(&a, ft_stack_new(j));
 			i++;
 		}
